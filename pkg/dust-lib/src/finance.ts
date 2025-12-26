93b71a9bbccbea0f908f9cbcbc9b80084bf094a2
charlyoleg <charles.braquet@gmx.fr>
2025-12-26 18:29:18 +0100

    fix format

--- pkg/dust-lib/src/finance.ts
+++ pkg/dust-lib/src/finance.ts
@@ -1,7 +1,12 @@
 // finance.ts
 // small functions around finance
 
-function calcLoan(iPrincipal: number, iInterestRate: number, iDuration:number, iTarget: number): number {
+function calcLoan(
+	iPrincipal: number,
+	iInterestRate: number,
+	iDuration: number,
+	iTarget: number
+): number {
 	let rAnnuity = 0;
 	if (!isNaN(iPrincipal) && !isNaN(iInterestRate) && !isNaN(iDuration) && !isNaN(iTarget)) {
 		rAnnuity = 1;
