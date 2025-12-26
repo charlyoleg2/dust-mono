2d786aae48401298f7e75cf7e942485eba36797a
charlyoleg <charles.braquet@gmx.fr>
2025-12-26 22:39:09 +0100

    add base in astro config

--- pkg/dust-ui/astro.config.mjs
+++ pkg/dust-ui/astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	base: '/dust-mono',
 	site: 'https://charlyoleg2.github.io/dust-mono/',
 	integrations: [
 		starlight({
@@ -23,5 +25,6 @@ export default defineConfig({
 				},
 			],
 		}),
+		sitemap(),
 	],
 });
