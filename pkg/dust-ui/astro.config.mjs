3a9f69c243ee047d67d9525dbe8ce6b615dc3b87
charlyoleg <charles.braquet@gmx.fr>
2025-12-26 22:16:30 +0100

    cosmetic

--- pkg/dust-ui/astro.config.mjs
+++ pkg/dust-ui/astro.config.mjs
@@ -6,8 +6,8 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'Dust info and apps',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/charlyoleg2/dust-mono' }],
 			sidebar: [
 				{
 					label: 'Guides',
